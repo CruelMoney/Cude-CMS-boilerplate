819228400e79436fc8d83a7cd1a8780cbef438f1
cruelmoney <chrdengso@gmail.com>
2017-06-18 01:39:37 +0200

    fetcher

--- src/Theme/index.js
+++ src/Theme/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Navigation from './blocks/Navigation/';
 import Homepage from './pages/HomePage/'
 import Footer from './blocks/Footer/';
-
+import fetcher from '../CMS/higher-order-components/Fetcher'
 import './assets/css/style.css'; //theme style
 
 class Index extends React.Component {
